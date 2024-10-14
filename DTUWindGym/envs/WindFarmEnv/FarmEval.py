a6cd7202beb315414fe4f593de074af07e540e0e
Marcus Binder Nilsen <manils@dtu.dk>
2024-10-14 13:46:28 +0000

    Add readme and example files alongside minor fixes

---
+++ DTUWindGym/envs/WindFarmEnv/FarmEval.py
@@ -0,0 +1,59 @@
+from .Wind_Farm_Env import WindFarmEnv
+
+
+"""
+This is a wrapper for the WindFarmEnv class. It is used to evaluate the environment with specific wind values.
+The difference is that we can set the wind values directly, and we can also set the yaw values directly.
+"""
+
+class FarmEval(WindFarmEnv):
+    metadata = {"render_modes": ["human", "rgb_array"]}
+
+    def __init__(self, turbine,
+                TI_min_mes:float = 0.0, TI_max_mes:float = 0.50, #Max and min values for the turbulence intensity measurements. Used for internal scaling
+                yaw_init = "Zeros", 
+                TurbBox = "Default",
+                yaml_path = None,
+                render_mode=None, seed = None,
+                ):
+        
+        #TODO There must be a better way to set all these valuesm **kwargs???
+        #Run the Env with these values, to make sure that the oberservartion space is the same.
+        super().__init__(turbine=turbine, 
+                         TI_min_mes=TI_min_mes, TI_max_mes=TI_max_mes,
+                         yaml_path=yaml_path,
+                         yaw_init=yaw_init, 
+                         TurbBox=TurbBox,
+                         Baseline_comp = True, #We always want to compare to the baseline, so this is true
+                         seed=seed,
+                         )
+        
+    def set_wind_vals(self, ws=None, ti=None, wd=None):
+        """
+        Set the wind values to be used in the evaluation
+        """
+        if ws is not None:
+            self.ws = ws
+            self.ws_min = ws
+            self.ws_max = ws
+        if ti is not None:
+            self.ti = ti
+            self.ti_min = ti
+            self.ti_max = ti
+        if wd is not None:
+            self.wd = wd
+            self.wd_min = wd
+            self.wd_max = wd
+
+    def set_yaw_vals(self, yaw_vals):
+        """
+        Set the yaw values to be used in the evaluation
+        """
+        self.yaw_initial = yaw_vals
+
+    def update_tf(self, path):
+        """
+        Overwrite the _def_site method to set the turbulence field to the path given
+        """
+        self.TF_files = [path]
+    
--- DTUWindGym/envs/WindFarmEnv/PyWakeAgent.py
+++ DTUWindGym/envs/WindFarmEnv/PyWakeAgent.py
@@ -1,7 +1,6 @@
 import numpy as np
 from py_wake.examples.data.lillgrund import LillgrundSite
 from py_wake.deflection_models.jimenez import JimenezWakeDeflection
-# from py_wake.deficit_models.gaussian import BastankhahGaussian, BlondelSuperGaussianDeficit2023
 
 from topfarm.cost_models.cost_model_wrappers import CostModelComponent
 from topfarm import TopFarmProblem
@@ -14,13 +13,20 @@ from py_wake.turbulence_models import CrespoHernandez
 
 import matplotlib.pyplot as plt
 
+
+"""
+The PyWakeAgent is a class that is used to optimize the yaw angles of a wind farm using the PyWake library.
+It interfaces with the AgentEval class in the dtu_wind_gym library.
+Based on the global wind conditons it can optimize the yaw angles and then use them during the simulation.
+"""
+
 class PyWakeAgent():
     def __init__(self, x_pos, y_pos,
                  wind_speed = 8, wind_dir = 270, TI=0.07,yaw_max=45, 
                  yaw_min=-45, maxiter=100, tol=1e-4, ec=1e-4, 
                  turbine = V80()):
-        self.pywakeagent = True #This is used in a hasattr. Dont ask me why. Im bad at python. 
-        self.optimized = False #Is false before we have optimized the farm.
+        self.pywakeagent = True     #This is used in a hasattr in the AgentEval class. 
+        self.optimized = False      #Is false before we have optimized the farm.
         self.maxiter=maxiter
         self.tol=tol
         self.ec=ec
@@ -36,7 +42,7 @@ class PyWakeAgent():
         self.turbine = turbine
 
         self.x_pos = x_pos
-        self.y_pos = y_pos #Note we move the farm 200 units up. This is done because I think I saw some weird behaviour with y = 0 :/ 
+        self.y_pos = y_pos 
         self.n_wt = len(x_pos)
 
         site.initial_position = np.array([x_pos, y_pos]).T
@@ -44,24 +50,30 @@ class PyWakeAgent():
         self.wf_model = Blondel_Cathelain_2020(site, turbine, turbulenceModel=CrespoHernandez(), deflectionModel=JimenezWakeDeflection())
 
         self.yaw_zero = np.zeros((self.n_wt,1,1))  #initial condition of yaw angles
-        #Define the farm, the wind farm model and the initial yaw angles
         self.reset()
 
     def power_func(self, yaw_ilk):
+        """
+        Function to calculate the power output of the wind farm.
+        """
         simres = self.wf_model(self.x_pos, self.y_pos, wd=self.wdir, 
                                  ws=self.wsp, yaw=yaw_ilk, tilt=0)
-        # power = simres.Power.sum()
         aep = simres.aep().sum()
         return aep
 
     def update_wind(self, wind_speed, wind_direction, TI):
+        """
+        Update the wind conditions for the agent.
+        """
         self.wsp = np.asarray([wind_speed])
         self.wdir = np.asarray([wind_direction])
         self.TI = TI
         self.reset()
 
     def reset(self):
-        #Reset the wind things for the objective. 
+        """
+        Reset the wind things for the objective. 
+        """
         self.optimized = False
 
         self.pywakefarm = self.wf_model(self.x_pos, self.y_pos, 
@@ -84,30 +96,35 @@ class PyWakeAgent():
                                 )
 
     def optimize(self):
+        """
+        Optimizes the yaw angles of the wind farm.
+        """
         _, state, self.info = self.problem.optimize()
         self.optimized = True #Now the farm has been optimized
         self.optimized_yaws = state['yaw_ilk'][:,0,0]
 
-    def update_wind(self, wind_speed, wind_direction, TI):
-        #Update the windconditions for the agent. Then reset the agent, so it is ready for optimization. 
-        self.wsp = np.asarray([wind_speed])
-        self.wdir = np.asarray([wind_direction])
-        self.TI = TI
-        self.reset()
-
     def scale_yaw(self):
-        #Make the scaling of the yaw angles.
-        #Scaled from [max, min] to [-1, 1]
+        """
+        Scale the yaw angles to be between -1 and 1.
+        """
         self.action = (self.optimized_yaws-self.yaw_min)/(self.yaw_max-self.yaw_min)*2-1
 
-    def predict(self, *args, **kwargs): #This is to make the agent compatible with the agent framework.
-        #Note that we dont use the obs or the deterministic arguments.
+    def predict(self, *args, **kwargs): 
+        """
+        This class pretends to be an agent, so we need to have a predict function.
+        If we havent called the optimize function, we do that now, and return the action
+        Note that we dont use the obs or the deterministic arguments.
+        """
+
         if self.optimized == False:
             self.optimize()
             self.scale_yaw()
-        return self.action, None #Return the optimized (scaled) yaws and None as to make it fit with the agent framework.
+        return self.action, None 
     
     def plot_flow(self):
+        """
+        Plot the flowfield of the wind farm.
+        """
         if self.optimized == False:
             self.optimize()
         simulationResult = self.wf_model(self.x_pos,self.y_pos,wd=self.wdir, ws=self.wsp, yaw=self.optimized_yaws, tilt=0)
