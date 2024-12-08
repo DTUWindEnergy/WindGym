93125d68eb035b45d3cf2ebbf8779bbd7804ca5e
Julian Quick <juqu@dtu.dk>
2024-12-08 15:04:36 +0100

    free all agents

--- DTUWindGym/envs/WindFarmEnv/Agents/__init__.py
+++ DTUWindGym/envs/WindFarmEnv/Agents/__init__.py
@@ -4,3 +4,6 @@ Base container for all "basic" agents.
 # from .PyWakeAgent import PyWakeAgent
 from .GreedyAgent import GreedyAgent
 from .FlorisAgent import FlorisAgent
+from .PyWakeAgent import PyWakeAgent
+from .BaseAgent import BaseAgent
+from .RandomAgent import RandomAgent
