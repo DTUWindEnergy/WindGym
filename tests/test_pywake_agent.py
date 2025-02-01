d50b0a7a15755e4be0fa855464a7305613caca2f
Julian Quick <juqu@dtu.dk>
2025-02-01 12:41:09 +0100

    formatting

--- tests/test_pywake_agent.py
+++ tests/test_pywake_agent.py
@@ -12,6 +12,7 @@ from py_wake.examples.data.hornsrev1 import V80
 from py_wake.literature.gaussian_models import Blondel_Cathelain_2020
 from py_wake.turbulence_models import CrespoHernandez
 
+
 def test_power_optimization():
     # Initialize parameters
     x_pos = [0, 500]
@@ -19,7 +20,7 @@ def test_power_optimization():
     wind_speed = 6
     wind_dir = 270
     TI = 0.02
-    
+
     # Setup wind farm models
     site = LillgrundSite()
     turbine = V80()
@@ -29,17 +30,21 @@ def test_power_optimization():
         turbulenceModel=CrespoHernandez(),
         deflectionModel=JimenezWakeDeflection(),
     )
-    
+
     def compute_power(yaws):
-        return wf_model(x=x_pos, y=y_pos, ws=wind_speed, wd=wind_dir, TI=TI, tilt=0, yaw=yaws).Power.sum()
-    
+        return wf_model(
+            x=x_pos, y=y_pos, ws=wind_speed, wd=wind_dir, TI=TI, tilt=0, yaw=yaws
+        ).Power.sum()
+
     # Compute nominal power
     nominal_power = compute_power([30, 0])
-    
+
     # Run optimization
-    agent = PyWakeAgent(x_pos=x_pos, y_pos=y_pos, wind_speed=wind_speed, wind_dir=wind_dir, TI=TI)
+    agent = PyWakeAgent(
+        x_pos=x_pos, y_pos=y_pos, wind_speed=wind_speed, wind_dir=wind_dir, TI=TI
+    )
     agent.optimize()
     agent_power = compute_power(agent.optimized_yaws)
-    
+
     # Assert optimization improved power
     assert agent_power >= nominal_power
