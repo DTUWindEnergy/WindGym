0d7f0985cf24418d320c1b97e61ff30916ff1c05
Julian Quick <juqu@dtu.dk>
2025-02-13 12:51:16 +0100

    saving a "blessed" setup

--- run.sh
+++ run.sh
@@ -13,4 +13,4 @@ conda activate marcus
 echo "============================================================================="
 
 #python examples/fulltime.py --dt_env 1 --power_avg 40 --seed 123  --yaml_path DTUWindGym/envs/WindFarmEnv/Examples/EnvConfigs/2turb.yaml --turbbox_path Hipersim_mann_l5.0_ae1.0000_g0.0_h0_128x128x128_3.000x3.00x3.00_s0001.nc --lookback_window 100 --learning_rate 3e-4 --momentum 0.
-python examples/curriculum.py --dt_env 2 --power_avg 40 --seed 123  --yaml_path DTUWindGym/envs/WindFarmEnv/Examples/EnvConfigs/2turb.yaml --turbbox_path Hipersim_mann_l5.0_ae1.0000_g0.0_h0_128x128x128_3.000x3.00x3.00_s0001.nc --lookback_window 100 --learning_rate 3e-4 --momentum 0. --n_passthrough 3 --penalty_mult 5
+python examples/curriculum.py --dt_env 2 --power_avg 40 --seed 123  --yaml_path DTUWindGym/envs/WindFarmEnv/Examples/EnvConfigs/2turb.yaml --turbbox_path Hipersim_mann_l5.0_ae1.0000_g0.0_h0_128x128x128_3.000x3.00x3.00_s0001.nc --lookback_window 100 --learning_rate 3e-4 --momentum 0. --n_passthrough 3 --penalty_mult 50 --similarity_type basic --train_steps 1000000 #--epochs_per_update 111 --n_steps 1024 --batch_size 256
