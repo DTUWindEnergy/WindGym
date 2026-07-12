2bd1a736c89f9bc4908420b100e70cc18b358ded
Marcus Binder <marcusbinder2@gmail.com>
2026-07-12 10:56:20 +0100

    Update __init__.py

--- WindGym/core/__init__.py
+++ WindGym/core/__init__.py
@@ -11,6 +11,7 @@ from .turbulence_manager import TurbulenceManager
 from .renderer import WindFarmRenderer
 from .baseline_manager import BaselineManager
 from .probe_manager import ProbeManager
+from .power_tracking import PowerTrackingManager
 from .mes_class import Mes, TurbMes, FarmMes
 from .wind_probe import WindProbe
 from .measurement_manager import (
@@ -33,6 +34,7 @@ __all__ = [
     "WindFarmRenderer",
     "BaselineManager",
     "ProbeManager",
+    "PowerTrackingManager",
     "Mes",
     "TurbMes",
     "FarmMes",
